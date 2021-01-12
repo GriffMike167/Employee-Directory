647bd83eb854274967507f21f3ad88a6ade40f42
Michael Griffith <griffithcmichael@gmail.com>
2021-01-11 19:42:50 -0600

    Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css"
+
+function Header () {
+    return (
+        <div className="header">
+            <h1>Employees Directory</h1>
+            <p>Click on each heading below to filter or user the search box to conduct your own search</p>
+        </div>
+    )
+}
+
+export default Header;
