5b789ee1d561985bea436a33dc4a65446eb7f961
Michael Griffith <griffithcmichael@gmail.com>
2021-01-11 20:04:15 -0600

    DataTable

---
+++ src/components/DataTable/index.js
@@ -0,0 +1,37 @@
+import REACT from "react";
+import DataBody from ",,/DataBody";
+import "./DataTable.css";
+import DataAreaContext from "../../utils/DataAreaContext";
+
+const DataTable = () => {
+    const context = useContext(DataAreaContext)
+
+    return (
+        <div className="datatable mt-2">
+            <table id="table"
+            calssName="table-hover">
+                <thead>
+                    <tr>
+                        {context.developerState.headings.map(({ name, width}) => {
+                            return (<th 
+                                className="col"
+                                key={name}
+                                style ={{ width}}
+                                onClick={() => {
+                                    context.handleSort(name.toLowerCase());
+                                }}
+                                >
+                                    { name }
+                                    <span className="pointer"></span>
+                                </th>
+                            );
+                        })}
+                    </tr>
+                </thead>
+
+                <DataBody />
+            </table>
+        </div>
+    )
+}
+export default DataTable
